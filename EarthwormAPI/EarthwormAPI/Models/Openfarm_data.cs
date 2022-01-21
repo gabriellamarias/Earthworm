2b602451a032e41b94700e12616fbbc5e7ef0aa4
gabi <gabriella.marias@gmail.com>
2022-01-20 22:08:14 -0500

    Added GrowStuff API

---
+++ EarthwormAPI/EarthwormAPI/Models/Openfarm_data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EarthwormAPI.Models
+{
+    public class Openfarm_data
+    {
+        public Attribute attribute { get; set; }
+    }
+}
